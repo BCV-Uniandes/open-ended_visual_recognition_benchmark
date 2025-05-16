0b13d25db6fb0ec2a146cfd1433aa0daf7912303
cigonzalez <ci.gonzalez10@uniandes.edu.co>
2025-05-16 06:17:00 +0000

    Change metric name

--- ALA/eval_ala.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python -m ALA.eval_open_vocab_seg_full_metric_open \
-    --dataset ade \
-    --model maskclip \
-    --model_outputs_path /home/cigonzalez/code/open-ended_visual_recognition_benchmark/outputs/maskclip/ade20k/descriptions.json \
-    --semantic_relationship_file_path ./ALA//semantic_relationships/output_semantics_gpt4_ade20k.json \
-    --num-gpus 1 \
-
